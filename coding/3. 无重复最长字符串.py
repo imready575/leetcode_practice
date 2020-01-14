2aac7d8102c95c01637f1bdf4eb22943908e5a54
e <weibanana55@163.com>
2020-01-14 18:01:36 +0800

    无重复最长字符串

--- coding/3. 无重复最长字符串.py
+++ coding/3. 无重复最长字符串.py
@@ -5,8 +5,27 @@
 输出: 3
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 """
+
+
+# 滑动窗口
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s):
         if not s:
             return 0
-
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len:
+                max_len = cur_len
+            lookup.add(s[i])
+        return max_len
